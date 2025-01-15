140e473401170540322728e1d71e7ba41c161fcc
Pro100D <114569004+Pro100D@users.noreply.github.com>
2025-01-15 15:31:53 +0100

    refactor: switch base_url local fo render

--- serializer/serializer.py
+++ serializer/serializer.py
@@ -1,5 +1,5 @@
 def convert_recipe(recipe) -> dict:
-    base_url = "http://127.0.0.1:8000"
+    base_url = "https://cooking-book0bd.onrender.com"
     image_url = f"{base_url}/images/{recipe['image']}" if "image" in recipe else None
     return {
         "id": str(recipe["_id"]),
